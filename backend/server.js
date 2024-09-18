cefea0d04aa198f8eb98400ec8b6794d02b778af
CristinaSuciu26 <134690607+CristinaSuciu26@users.noreply.github.com>
2024-09-18 15:12:33 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,13 @@
-import mongoose from "mongoose";
-import app from "./index.js";
+import app from "./index.js"; // Importing the express app
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 // Destructure environment variables with default port
-const { MONGO_URI, PORT = 3000 } = process.env;
+const { PORT = 3000 } = process.env;
 
-// Validate MONGO_URI
-if (!MONGO_URI) {
-  console.error("MONGO_URI is not defined");
-  process.exit(1);
-}
-
-// Connect to MongoDB and start the server
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(
-        `Database connection successful. Use your API on port ${PORT}`
-      );
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1);
-  });
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
