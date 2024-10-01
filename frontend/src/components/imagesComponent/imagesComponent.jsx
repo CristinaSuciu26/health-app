8704ca618e912c8ce2a3f9f70755a3b492dd0111
CristinaSuciu26 <134690607+CristinaSuciu26@users.noreply.github.com>
2024-10-01 18:37:19 +0300

    update images component

--- frontend/src/components/imagesComponent/imagesComponent.jsx
+++ frontend/src/components/imagesComponent/imagesComponent.jsx
@@ -1,47 +1,20 @@
 import React from "react";
 import styles from "./imagesComponent.module.css";
-import leafs from "../../assets/images/leafs.png";
-import leafsDesktop from "../../assets/images/leafs-desktop.png";
-import layer from "../../assets/images/layer.png";
-import layerDesktop from "../../assets/images/layer-desktop.png";
-import banana from "../../assets/images/banana.png";
-import bananaDesktop from "../../assets/images/banana-desktop.png";
-import strawberry from "../../assets/images/strawberry.png";
-import strawberryDesktop from "../../assets/images/strawberry-desktop.png";
 
-const ImagesComponent = () => {
+const ImagesComponent = ({ images, positions }) => {
   return (
-    <div>
-      <div className={styles.imagesContainer}>
-        <img className={styles.leafsImg} src={leafs} alt="leafs" />
+    <div className={styles.imagesContainer}>
+      {images.map((image, index) => (
         <img
-          className={styles.leafsDesktopImg}
-          src={leafsDesktop}
-          alt="leafs"
+          key={index}
+          className={styles[`image${index}`]}
+          src={image.src}
+          srcSet={`${image.src} 768w, ${image.srcDesktop} 1280w`}
+          sizes="(min-width: 1280px) 1280px, (min-width: 768px) 768px, 100vw"
+          alt={image.alt}
+          style={positions ? positions[index] : {}}
         />
-        <img className={styles.layerImg} src={layer} alt="layer" />
-        <img
-          className={styles.layerDesktopImg}
-          src={layerDesktop}
-          alt="layer"
-        />
-        <img className={styles.bananaImg} src={banana} alt="banana" />
-        <img
-          className={styles.bananaDesktopImg}
-          src={bananaDesktop}
-          alt="banana"
-        />
-        <img
-          className={styles.strawberryImg}
-          src={strawberry}
-          alt="strawberry"
-        />
-        <img
-          className={styles.strawberryDesktopImg}
-          src={strawberryDesktop}
-          alt="strawberry"
-        />
-      </div>
+      ))}
     </div>
   );
 };
--- frontend/src/pages/calculatorPage/CalculatorPage.jsx
+++ frontend/src/pages/calculatorPage/CalculatorPage.jsx
@@ -4,9 +4,30 @@ import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/authSelectors";
 import ImagesComponent from "../../components/imagesComponent/imagesComponent";
 import styles from "./CalculatorPage.module.css";
+import leafs from "../../assets/images/leafs.png";
+import leafsDesktop from "../../assets/images/leafs-desktop.png";
+import layer from "../../assets/images/layer.png";
+import layerDesktop from "../../assets/images/layer-desktop.png";
+import banana from "../../assets/images/banana.png";
+import bananaDesktop from "../../assets/images/banana-desktop.png";
+import strawberry from "../../assets/images/strawberry.png";
+import strawberryDesktop from "../../assets/images/strawberry-desktop.png";
 
 const CalculatorPage = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const images = [
+    { src: leafs, srcDesktop: leafsDesktop, alt: "Leafs" },
+    { src: layer, srcDesktop: layerDesktop, alt: "Layer" },
+    { src: banana, srcDesktop: bananaDesktop, alt: "Banana" },
+    { src: strawberry, srcDesktop: strawberryDesktop, alt: "Strawberry" },
+  ];
+  // This is optional
+  // const positions = [
+  //   { top: "-90px", right: "80px", position: "absolute", zIndex: "2" },
+  //   { top: "0px", right: "0px", position: "absolute" },
+  //   { top: "-24px", right: "0px", position: "absolute" },
+  //   { top: "218px", right: "-26px", position: "absolute" },
+  // ];
 
   return (
     <div className={styles.calculatorPageContainer}>
@@ -17,7 +38,7 @@ const CalculatorPage = () => {
       ) : (
         <>
           <DailyCaloriesForm />
-          <ImagesComponent />
+          <ImagesComponent images={images} />
         </>
       )}
     </div>
